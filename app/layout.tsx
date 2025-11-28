8e60360fb40009b445a7b915d99c95d7b50d4832
bansoshar <nakaima.bansosha@gmail.com>
2025-11-28 12:35:48 +0000

    update meta description and OGP tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 
 // メタデータの設定
 export const metadata: Metadata = {
-  title: '願いを諦めない人のライフコーチング｜NAKAIMA BANSOSHARナカイマ伴走舎',
+  title: '願いを諦めない人のライフコーチング｜ナカイマ伴走舎',
   description: '願いがあるのに動けなくなっているあなたへ。**対話とストレングスファインダーを通じて、**迷いを「納得の一歩」に変えるサポートをします。周囲の期待ではなく、自分の本音で決断したい方へ。まずは無料体験セッションから。',
   icons: {
     icon: '/icon.png', // または '/icon.png'
